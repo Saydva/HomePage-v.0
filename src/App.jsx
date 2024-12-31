6ec87308672af658aa91ce342d4bfc0f9ddb1059
Saydva <kacuikjozef@gmail.com>
2024-12-31 01:20:20 +0100

    starting building home page with React and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <div className='bg-emerald-300 m-3 p-3 rounded text-center'>dodo</div>
+      <p className="underline text-center">Hi there</p>
+    </>
+  )
+}
+
+export default App
