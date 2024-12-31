9b94e6b6ad0b0f2f014172071bde3f1f45e9bdc3
Saydva <kacuikjozef@gmail.com>
2024-12-31 10:15:45 +0100

    just testing components tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
+import React, { useState } from "react";
+
 function App() {
+  const [on, setOn] = useState("white");
+
+  const handleMode = (e) => {
+    e.preventDefault();
+    if (on != "dark") {
+      setOn("dark");
+    } else {
+      setOn("white");
+    }
+  };
 
   return (
-    <>
-      <div className='bg-emerald-300 m-3 p-3 rounded text-center'>dodo</div>
-      <p className="underline text-center">Hi there</p>
-    </>
-  )
+    <div className={on}>
+      <div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+        <div>
+          <span class="inline-flex items-center justify-center p-2 rounded-md shadow-lg">
+            <button
+              onClick={handleMode}
+              class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+            >
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Mode: {on}
+              </span>
+            </button>
+          </span>
+        </div>
+        <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
+          Writes Upside-Down
+        </h3>
+        <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+          The Zero Gravity Pen can be used to write in any orientation,
+          including upside-down. It even works in outer space.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
