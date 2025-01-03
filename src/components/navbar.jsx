e88be4bfb8797fb31bb94242d9241df2381e1572
Saydva <kacuikjozef@gmail.com>
2025-01-03 04:01:39 +0100

    buildin hero section n ow

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,15 +4,18 @@ function Navbar(props) {
   const changeTheme = props.changeTheme;
   const themeName = props.themeName;
   return (
-    <div className="flex">
+    <div className="flex justify-between max-sm:flex-wrap max-sm:justify-end ">
       <div className="navbar bg-base-100">
         <a className="btn btn-ghost text-xl">Home</a>
         <a className="btn btn-ghost text-xl">Projects</a>
         <a className="btn btn-ghost text-xl">About</a>
       </div>
-      <div className="flex items-end self-end gap-6">
-        <div className="stat-value text-lg pb-3">{themeName}</div>
-        <button className="btn btn-primary mr-14" onClick={changeTheme}>
+      <div className="flex items-center gap-6">
+        <div className="stat-value text-lg">{themeName}</div>
+        <button
+          className="btn btn-primary mr-14 max-sm:mr-0"
+          onClick={changeTheme}
+        >
           Change theme
         </button>
       </div>
